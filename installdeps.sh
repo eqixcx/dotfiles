e4317ba6fc06150eaa5ff8745279212e7c987303
Cooper Garner <cooper@eqix.tech>
2021-11-26 15:32:03 +0000

    yay testing branch

--- installdeps.sh
+++ installdeps.sh
@@ -52,10 +52,22 @@ sudo systemctl enable lightdm
 clear
 echo Installing and making Zsh default shell
 echo ---------------------------------------
-sudo pacman -S zsh
+sudo pacman -S zsh --noconfirm
+# TODO: ADD ZSH SWAP
 
 # install lxappearance and some icon and gtk themes 
 clear
 echo Installing lxappearance to change themes
 echo ----------------------------------------
-sudo pacman -S lxappearance-gtk3
+sudo pacman -S lxappearance-gtk3 --noconfirm
+
+## experimental
+# install yay (idk if this will work)
+clear
+echo Installing yay package manager
+echo ------------------------------
+mkdir /tmp/yayinstall
+cd /tmp/yayinstall
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
