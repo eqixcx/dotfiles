56f63c1667fe9489303a67fa941e8676dcee9a7e
Cooper Garner <cooper@eqix.tech>
2021-11-26 14:21:29 +0000

    install script - bluetooth, audio, lightdm
    yes

--- installdeps.sh
+++ installdeps.sh
@@ -13,6 +13,17 @@ echo Installing base xorg and i3-gaps
 echo --------------------------------
 sudo pacman -S xorg-server xorg-apps xorg-xinit i3-gaps i3status i3lock network-manager-applet --noconfirm
 
+# install pulseaudio and other utilities
+echo Installing Pulseaudio and other utilities
+echo -----------------------------------------
+sudo pacman -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth pasystray
+
+# install bluetooth drivers and frontend
+echo Installing Bluetooth and frontend
+echo ---------------------------------
+sudo pacman -S bluez bluez-utils
+sudo systemctl enable bluetooth.service
+
 # install other utilities that i like to use on arch
 clear
 echo Installing extra utilities I like to use on arch
@@ -28,11 +39,14 @@ sudo pacman -S lightdm lightdm-gtk-greeter --noconfirm
 # install neat fonts and stuff
 clear
 echo Installing extra fonts
+echo ----------------------
 sudo pacman -S noto-fonts ttf-ubuntu-font-family ttf-dejavu ttf-freefont ttf-liberation ttf-droid ttf-inconsolata ttf-roboto terminus-font ttf-font-awesome --noconfirm
 
 # enable lightdm as display manager
 clear
 echo Enabling LightDM as display manager
+echo -----------------------------------
+sudo systemctl enable lightdm
 
 # switch to using zsh as default shell
 clear
